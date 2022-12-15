54a6302c80210c6e0d56f9059cd7dca3632f65b1
Volodymyr Poliakov <vladimir03002@gmail.com>
2022-12-16 00:38:24 +0100

    modal trying

---
+++ src/index.js
@@ -0,0 +1,10 @@
+$('.js-about-open-modal').click(function (event) {
+  event.preventDefault();
+
+  var modalName = $(this).attr('data-modal');
+  var modal = $('js-about-modal[data-modal]');
+
+  .modal[data-modal="about-iframe"]
+});
+
+
